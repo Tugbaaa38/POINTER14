df25892d830a902b1e177032b40435a02e1e8981
Tugba <77529943+Tugbaaa38@users.noreply.github.com>
2021-03-18 14:59:44 +0300

    Add files via upload

---
+++ pointer14.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//DIZILERIN FONKSIYONLARA GOSTERICILER YOLUYLA GECIRILMESI
+
+void display_array(const int *ptr ,int size)
+{
+	int i;
+	for(i=0;i<size;i++)
+   {
+   	printf("%d ",*(ptr+i));
+   }
+}
+
+int main() {
+     //BILGI: const belirteci: Programcinin derleyiciye belirli bir degiskenin degerinin degistirilmemesi gerektiginin bildirilmesini saglar.
+     int boyut;
+	 int dizi[20],i;
+     printf("Boyut:");
+     scanf("%d",&boyut);
+	 printf("Diziyi doldurun\n");
+	 for(i=0;i<boyut;i++)
+	 {
+	 	printf("dizi[%d]=",i);
+	 	scanf("%d",&dizi[i]);
+	 }
+	 system("cls");
+	 display_array(dizi,boyut);
+
+	return 0;
+}
+
